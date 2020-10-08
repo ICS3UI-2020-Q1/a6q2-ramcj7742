23d0a87fa865c23db6915a906876e15e584275a0
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-08 17:45:06 +0000

    Finished

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 /**
- *
+ * gets 10 integers and finds the one the user selects
  * @author 
  */
 public class Main {
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //prompts user to enter 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //creates array
+    int array[] = new int[10];
+
+    //puts users numbers into array
+    for(int i = 0; i < array.length; i++){
+      array[i] = input.nextInt();
+    }
+
+    //prompts user to enter number to find
+    System.out.println("Please enter a number to find");
+    int userNum = input.nextInt();
+
+    //finds instances of that number in array and displays to user
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == userNum){
+        System.out.println(userNum + " is located at index " + i);
+      }
+    }
     
   }
 }
